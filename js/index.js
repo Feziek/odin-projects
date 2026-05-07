458135d5147092b255059c950204aaf6524da985
Feziek <josephclarksabado575@gmail.com>
2026-05-07 19:11:53 +0800

    feat: add Gameboard IIFE module with board state and actions

--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
 const createPlayer = (name, marker) => {
 	return { name, marker };
 };
+
+const Gameboard = (() => {
+	const board = new Array(9).fill(null);
+
+	const getBoard = () => [...board];
+	const placeMarker = (index, marker) => {
+		if (index < 0 || index > 8 || board[index] !== null) return false;
+		board[index] = marker;
+		return true
+	};
+	const resetBoard = () => board.fill(null);
+
+	return {
+		getBoard,
+		resetBoard,
+		placeMarker
+	};
+})();
