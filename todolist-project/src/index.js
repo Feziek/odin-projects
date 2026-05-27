f1a571b471f407cbb34baf184af0c96bd9fb93b7
Feziek <josephclarksabado575@gmail.com>
2026-05-27 23:02:49 +0800

    feat(todolist): add routing logic to pages

--- todolist-project/src/index.js
+++ todolist-project/src/index.js
@@ -1,1 +1,26 @@
-console.log('Hello from webpack!')
+import homePage from './pages/home.js';
+import projectPage from './pages/project.js';
+import taskPage from './pages/task.js';
+
+const navButtons = document.querySelectorAll('[data-page]');
+
+function navigate(page) {
+	const pages = {
+		home: homePage,
+		task: taskPage,
+		project: projectPage
+	};
+
+	if (pages[page]) {
+		pages[page]();
+	} else {
+		document.getElementById('content').innerHTML = '<h1>Page not Found!</h1>';
+	}
+}
+
+navButtons.forEach(btn => {
+  btn.addEventListener("click", () => navigate(btn.dataset.page))
+	
+});
+
+navigate('home');
