a7dc44fb7c1968275cd9fa1d86edd1b82045a380
Feziek <josephclarksabado575@gmail.com>
2026-05-29 23:32:10 +0800

    fix(todolist): remove this keyword on createTask factory function

--- todolist-project/src/services/taskService.js
+++ todolist-project/src/services/taskService.js
@@ -1,13 +1,13 @@
 import { STORAGE_KEY, getTodoData } from './storageService.js';
 
 function createTask(title, desc, dueDate, priority) {
-	this.id = crypto.randomUUID().slice(0, 8);
-	this.title = title;
-	this.desc = desc;
-	this.dueDate = dueDate;
-	this.priority = priority;
-
-	return { id, title, desc, dueDate, priority };
+	return {
+		id: crypto.randomUUID().slice(0, 8),
+		title,
+		desc,
+		dueDate,
+		priority
+	};
 }
 
 function addTask(title, desc, dueDate, priority) {
