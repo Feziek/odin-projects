8d980730babb2510d5057156e6222745dff67bd0
Feziek <josephclarksabado575@gmail.com>
2026-05-29 23:14:57 +0800

    feat(todolist): add createTask factory function for task object generation and integrate it on addTask function

--- todolist-project/src/services/taskService.js
+++ todolist-project/src/services/taskService.js
@@ -1,7 +1,18 @@
 import { STORAGE_KEY, getTodoData } from './storageService.js';
 
-function addTask(taskObj) {
-	const data = getTodoData();
-	data.tasks.push(taskObj);
+function createTask(title, desc, dueDate, priority) {
+	this.id = crypto.randomUUID().slice(0, 8);
+	this.title = title;
+	this.desc = desc;
+	this.dueDate = dueDate;
+	this.priority = priority;
+
+	return { id, title, desc, dueDate, priority };
+}
+
+function addTask(title, desc, dueDate, priority) {
+	data = getTodoData();
+	const task = createTask(title, desc, dueDate, priority);
+	data.tasks.push(task);
 	localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
 }
