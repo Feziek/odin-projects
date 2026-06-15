637e17e7f8587b87682c45d7cb23da1c5ce10fda
Feziek <josephclarksabado1@gmail.com>
2026-06-16 00:10:25 +0800

    perf(weather-app): optimize webpack dev config for faster startup

--- weather-app/webpack.dev.js
+++ weather-app/webpack.dev.js
@@ -3,8 +3,11 @@ import common from './webpack.common.js';
 
 export default merge(common, {
 	mode: 'development',
-	devtool: 'eval-source-map',
+	devtool: 'eval-cheap-module-source-map',
 	devServer: {
 		watchFiles: ['./src/template.html'],
 	},
+	cache: {
+		type: "filesystem"
+	}
 });
