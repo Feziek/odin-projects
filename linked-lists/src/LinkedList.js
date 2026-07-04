1120699fb486bc2327ff0db1b4cca457583749e6
Feziek <josephclarksabado1@gmail.com>
2026-07-04 19:26:31 +0800

    feat(linked-lists): implement append method for LinkedList class

--- linked-lists/src/LinkedList.js
+++ linked-lists/src/LinkedList.js
@@ -7,4 +7,18 @@ class Node {
 
 class LinkedList {
   #head = null;
+
+  append(value) {
+    if (this.#head === null) {
+      this.#head = new Node(value);
+      return;
+    }
+
+    let current = this.#head;
+
+    while (current.nextNode !== null) {
+      current = current.nextNode;
+    }
+    current.nextNode = new Node(value);
+  }
 }
