a301c195364ed8a473f82a17a2fa2520bbc15590
Feziek <josephclarksabado1@gmail.com>
2026-07-07 18:58:33 +0800

    feat(hashmap): implement hash method for key hashing functionality

--- hashmap-project/src/Hashmap.js
+++ hashmap-project/src/Hashmap.js
@@ -4,4 +4,16 @@ class HashMap {
   #loadFactor = 0.75;
   #capacity = 16;
   #buckets = Array.from({ length: this.#capacity }, () => new LinkedList());
+
+  hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.#capacity;
+    }
+
+    return hashCode;
+  }
 }
+
