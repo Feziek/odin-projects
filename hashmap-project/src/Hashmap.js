3fa81042d849132a2b103071ca73d6d50b4c4e98
Feziek <josephclarksabado1@gmail.com>
2026-07-09 11:33:06 +0800

    style(hashmap): format code for consistency and readability

--- hashmap-project/src/Hashmap.js
+++ hashmap-project/src/Hashmap.js
@@ -1,41 +1,41 @@
-import LinkedList from './LinkedList.js';
+import LinkedList from "./LinkedList.js";
 
 class HashMap {
-	#loadFactor = 0.75;
-	#capacity = 16;
-	#buckets = Array.from({ length: this.#capacity }, () => new LinkedList());
+  #loadFactor = 0.75;
+  #capacity = 16;
+  #buckets = Array.from({ length: this.#capacity }, () => new LinkedList());
 
-	hash(key) {
-		let hashCode = 0;
+  hash(key) {
+    let hashCode = 0;
 
-		const primeNumber = 31;
-		for (let i = 0; i < key.length; i++) {
-			hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.#capacity;
-		}
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.#capacity;
+    }
 
-		return hashCode;
-	}
+    return hashCode;
+  }
 
-	set(key, value) {
-		const list = this.#buckets[this.hash(key)];
-		const node = list.find(key);
+  set(key, value) {
+    const list = this.#buckets[this.hash(key)];
+    const node = list.find(key);
 
-		if (node) {
-			node.value.value = value;
-			return;
-		}
+    if (node) {
+      node.value.value = value;
+      return;
+    }
 
-		list.append({ key, value });
-	}
+    list.append({ key, value });
+  }
 
-	get(key) {
-		const list = this.#buckets[this.hash(key)];
-		const node = list.find(key);
+  get(key) {
+    const list = this.#buckets[this.hash(key)];
+    const node = list.find(key);
 
-		if (node) {
-			return node.value.value;
-		}
+    if (node) {
+      return node.value.value;
+    }
 
-		return null;
-	}
+    return null;
+  }
 }
