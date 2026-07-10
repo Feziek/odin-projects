2bca0f6ca45162ebc1e1a4ae51c28519a163de37
Feziek <josephclarksabado1@gmail.com>
2026-07-10 17:23:01 +0800

    refactor(linkedlist): remove unused methods and clean up code

--- hashmap-project/src/LinkedList.js
+++ hashmap-project/src/LinkedList.js
@@ -22,17 +22,6 @@ class LinkedList {
     current.nextNode = new Node(value);
   }
 
-  prepend(value) {
-    if (this.#head === null) {
-      this.#head = new Node(value);
-      return;
-    }
-
-    let temp = this.#head;
-    this.#head = new Node(value);
-    this.#head.nextNode = temp;
-  }
-
   size() {
     let current = this.#head;
     let count = 0;
@@ -43,30 +32,18 @@ class LinkedList {
     return count;
   }
 
-  head() {
-    if (this.#head === null) return undefined;
-    return this.#head.value;
-  }
-
   isHead(value) {
     if (this.#head.value.key === value) return true;
     return false;
   }
 
-  tail() {
-    if (this.#head === null) return undefined;
-    let current = this.#head;
-    while (current.nextNode !== null) {
-      current = current.nextNode;
-    }
-    return current.value;
-  }
-
   at(index) {
     if (index === 0) return this.#head;
     if (index < 0) return undefined;
+
     let current = this.#head;
     let position = 0;
+
     while (position < index) {
       current = current.nextNode;
       if (!current) return undefined;
@@ -76,16 +53,10 @@ class LinkedList {
     return current;
   }
 
-  pop() {
-    if (this.#head === null) return undefined;
-    const value = this.#head.value;
-    this.#head = this.#head.nextNode;
-    return value;
-  }
-
   contains(value) {
     if (this.#head === null) return false;
     let current = this.#head;
+
     while (current.nextNode !== null) {
       if (current.value.key === value) return true;
       current = current.nextNode;
@@ -104,20 +75,6 @@ class LinkedList {
     }
     if (current.value.key === value) return index;
     return -1;
-  }
-
-  toString() {
-    let string = "";
-    if (this.#head === null) return "";
-    let current = this.#head;
-
-    while (current.nextNode !== null) {
-      string += `( ${JSON.stringify(current.value)} ) -> `;
-      current = current.nextNode;
-    }
-
-    string += `( ${JSON.stringify(current.value)} ) -> null`;
-    return string;
   }
 
   find(value) {
