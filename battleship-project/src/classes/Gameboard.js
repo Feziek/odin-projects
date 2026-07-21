ee34255e1ea25ea643c96f4f7973704ee038d15c
Feziek <josephclarksabado1@gmail.com>
2026-07-21 11:24:14 +0800

    feat(battleship): implement placeShip and validation methods in Gameboard class

--- battleship-project/src/classes/Gameboard.js
+++ battleship-project/src/classes/Gameboard.js
@@ -4,6 +4,52 @@ class Gameboard {
 	board = Array.from({ length: 10 }, () =>
 		Array.from({ length: 10 }, () => ({ ship: null, isHit: false })),
 	);
+
+	placeShip(coords, length, isHorizontal) {
+		if (!this.#isValidPosition(coords, length, isHorizontal)) return;
+
+		const ship = new Ship(length);
+		const cells = this.#getCells(coords, length, isHorizontal);
+
+		for (const cell of cells) {
+			const [x, y] = cell;
+			this.board[x][y].ship = ship;
+		}
+	}
+
+	#isValidPosition(coords, length, isHorizontal) {
+		const [startX, startY] = coords;
+
+		if (isHorizontal) {
+			if (startY + length - 1 > 9) return false;
+		} else {
+			if (startX + length - 1 > 9) return false;
+		}
+
+		const cells = this.#getCells(coords, length, isHorizontal);
+
+		for (const cell of cells) {
+			const [x, y] = cell;
+			if (this.board[x][y].ship) return false;
+		}
+
+		return true;
+	}
+
+	#getCells(coords, length, isHorizontal) {
+		let result = [];
+		const [x, y] = coords;
+
+		for (let i = 0; i < length; i++) {
+			if (isHorizontal) {
+				result.push([x, y + i]);
+			} else {
+				result.push([x + i, y]);
+			}
+		}
+
+		return result;
+	}
 }
 
 export default Gameboard;
