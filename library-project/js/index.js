b28a0d649780086b89396fdf10ad659b01667888
Feziek <josephclarksabado1@gmail.com>
2026-08-13 12:36:00 +0800

    style(library-project): improve indentation and formatting for HTML, CSS, and JavaScript files

--- library-project/js/index.js
+++ library-project/js/index.js
@@ -11,33 +11,33 @@ const pagesInput = document.querySelector('#pages');
 const library = [];
 
 class Book {
-	constructor(title, author, pages) {
-		this.title = title;
-		this.author = author;
-		this.id = crypto.randomUUID().slice(0, 8);
-		this.pages = pages;
-	}
+  constructor(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.id = crypto.randomUUID().slice(0, 8);
+    this.pages = pages;
+  }
 }
 
 function addBookToLibrary(title, author, pages) {
-	const newBook = new Book(title, author, pages);
-	library.push(newBook);
-	renderBook();
+  const newBook = new Book(title, author, pages);
+  library.push(newBook);
+  renderBook();
 }
 
 function renderBook() {
-	if (library.length === 0) return;
+  if (library.length === 0) return;
 
-	const getStarted = document.querySelector('.get-started');
-	if (getStarted) getStarted.remove();
+  const getStarted = document.querySelector('.get-started');
+  if (getStarted) getStarted.remove();
 
-	libraryContainer.innerHTML = '';
+  libraryContainer.innerHTML = '';
 
-	library.forEach(book => {
-		const { title, author, pages, id } = book;
-		const card = document.createElement('div');
-		card.classList.add('book-card');
-		card.innerHTML = `
+  library.forEach((book) => {
+    const { title, author, pages, id } = book;
+    const card = document.createElement('div');
+    card.classList.add('book-card');
+    card.innerHTML = `
       <p class="status">• Available</p>
       <h2>${title}</h2>
       <div class="book-details-row">
@@ -53,63 +53,63 @@ function renderBook() {
         <p>${pages}</p>
       </div>
     `;
-		libraryContainer.appendChild(card);
-	});
+    libraryContainer.appendChild(card);
+  });
 }
 
 function openModal() {
-	modalContainer.classList.add('open');
-	libraryContainer.style.filter = 'blur(3px)';
-	header.style.filter = 'blur(3px)';
+  modalContainer.classList.add('open');
+  libraryContainer.style.filter = 'blur(3px)';
+  header.style.filter = 'blur(3px)';
 }
 
 function closeModal() {
-	modalContainer.classList.remove('open');
-	libraryContainer.style.filter = 'blur(0px)';
-	header.style.filter = 'blur(0px)';
-	modal.reset();
+  modalContainer.classList.remove('open');
+  libraryContainer.style.filter = 'blur(0px)';
+  header.style.filter = 'blur(0px)';
+  modal.reset();
 }
 
 function showError(field) {
-	const errorMsg = document.querySelector(`#${field} + span`);
-	const input = document.querySelector(`#${field}`);
-	const header = document.querySelector(`label[for="${field}"]`);
-	if (input.validity.valueMissing) {
-		errorMsg.textContent = `This field is required`;
-		input.classList.add('invalid');
-		header.classList.add('invalid');
-	} else {
-		errorMsg.textContent = '';
-		input.classList.remove('invalid');
-		header.classList.remove('invalid');
-	}
+  const errorMsg = document.querySelector(`#${field} + span`);
+  const input = document.querySelector(`#${field}`);
+  const header = document.querySelector(`label[for="${field}"]`);
+  if (input.validity.valueMissing) {
+    errorMsg.textContent = `This field is required`;
+    input.classList.add('invalid');
+    header.classList.add('invalid');
+  } else {
+    errorMsg.textContent = '';
+    input.classList.remove('invalid');
+    header.classList.remove('invalid');
+  }
 }
 
 titleInput.addEventListener('blur', () => {
-	showError('title');
+  showError('title');
 });
 authorInput.addEventListener('blur', () => {
-	showError('author');
+  showError('author');
 });
 pagesInput.addEventListener('blur', () => {
-	showError('pages');
+  showError('pages');
 });
 
 function submitBookInfo(e) {
-	e.preventDefault();
-	if (!modal.checkValidity()) {
-		console.log('i have field that is empty');
-		return;
-	}
+  e.preventDefault();
+  if (!modal.checkValidity()) {
+    console.log('i have field that is empty');
+    return;
+  }
 
-	const title = document.getElementById('title').value.trim();
-	const author = document.getElementById('author').value.trim();
-	const pages = document.getElementById('pages').value.trim();
+  const title = document.getElementById('title').value.trim();
+  const author = document.getElementById('author').value.trim();
+  const pages = document.getElementById('pages').value.trim();
 
-	addBookToLibrary(title, author, pages);
-	closeModal();
+  addBookToLibrary(title, author, pages);
+  closeModal();
 }
 
 addBookBtn.addEventListener('click', openModal);
 closeBtn.addEventListener('click', closeModal);
-modal.addEventListener('submit', e => submitBookInfo(e));
+modal.addEventListener('submit', (e) => submitBookInfo(e));
