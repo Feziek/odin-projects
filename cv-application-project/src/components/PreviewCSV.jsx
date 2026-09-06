5623c30248ae0d0ccaed3055b6f27fc61285bb2b
Feziek <josephclarksabado1@gmail.com>
2026-09-06 22:30:05 +0800

    feat(cv-application-project): add PreviewCSV component for displaying user information

---
+++ cv-application-project/src/components/PreviewCSV.jsx
@@ -0,0 +1,13 @@
+function PreviewCSV({ name, email, contact }) {
+  return (
+    <div className='preview'>
+      <section className='general-info'>
+        <p>Name: {name}</p>
+        <p>Email: {email}</p>
+        <p>Contact: {contact}</p>
+      </section>
+    </div>
+  );
+}
+
+export default PreviewCSV;
