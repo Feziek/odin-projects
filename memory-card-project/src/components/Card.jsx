b2fd28bc655e7675af1d0bac4cf8fc7fb0a41af4
Feziek <josephclarksabado1@gmail.com>
2026-09-19 20:11:13 +0800

    feat(memory-game-project): add Card component for memory game with click handling

---
+++ memory-card-project/src/components/Card.jsx
@@ -0,0 +1,8 @@
+export default function Card({ id, name, image, onClick }) {
+  return (
+    <div className='card' onClick={() => onClick(id)}>
+      <img src={image} alt={name} />
+      <p>{name}</p>
+    </div>
+  );
+}
