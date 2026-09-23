ce773bdfcef91a797cbee3157bdcfb583b56f0ca
Feziek <josephclarksabado1@gmail.com>
2026-09-23 22:34:49 +0800

    refactor(memory-card-project): refactor App and Deck components for improved game logic and card handling

--- memory-card-project/src/App.jsx
+++ memory-card-project/src/App.jsx
@@ -1,34 +1,77 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Deck from './components/Deck';
 import Scoreboard from './components/Scoreboard';
+
+const POKEMON_IDS = [1, 22, 13, 4, 5, 99, 2, 18, 19, 20, 11, 7];
 
 export default function App() {
   const [highestScore, setHighestScore] = useState(0);
   const [currentScore, setCurrentScore] = useState(0);
+  const [cards, setCards] = useState([]);
+  const [clickedCards, setClickedCards] = useState(new Set());
 
-  function handleGameUpate(gameInfo) {
-    const { status, score } = gameInfo;
+  useEffect(() => {
+    const fetchPokemon = async () => {
+      const response = await Promise.all(
+        POKEMON_IDS.map((id) =>
+          fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then((res) =>
+            res.json(),
+          ),
+        ),
+      );
 
-    switch (status) {
-      case 'lose':
-        console.log('You lose');
-        if (highestScore < score) setHighestScore(score);
-        break;
-      case 'win':
-        console.log('You win');
-        setHighestScore(0);
-        break;
-      case 'valid':
-        console.log('Valid move');
-        setCurrentScore(score);
-        break;
+      const cardData = response.map((item) => {
+        return {
+          id: item.id,
+          name: item.name,
+          image:
+            item.sprites.other['official-artwork'].front_default ??
+            item.sprites.front_default,
+        };
+      });
+
+      setCards(cardData);
+    };
+
+    fetchPokemon();
+  }, []);
+
+  function shuffleCards() {
+    const array = [...cards];
+
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
     }
+
+    setCards(array);
+  }
+
+  function handleClick(id) {
+    if (clickedCards.has(id)) {
+      console.log('game over');
+      if (highestScore < currentScore) setHighestScore(currentScore);
+      return;
+    }
+
+    if (clickedCards.size + 1 === POKEMON_IDS.length) {
+      console.log('you won');
+      setHighestScore(0);
+      return;
+    }
+
+    const newClickedCards = new Set(clickedCards);
+    newClickedCards.add(id);
+
+    setCurrentScore((prev) => prev + 1);
+    setClickedCards(newClickedCards);
+    shuffleCards();
   }
 
   return (
     <>
       <Scoreboard currentScore={currentScore} highestScore={highestScore} />
-      <Deck onGameUpdate={handleGameUpate} />
+      <Deck onCardClick={handleClick} cards={cards} />
     </>
   );
 }
